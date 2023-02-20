3eb2239456ce9a3cde7466a957d66ab16310bb6f
duy.nguyen_huu@napaglobal.com <duy.nguyen_huu>
2023-02-20 11:39:25 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
+const api = [
+  {
+    id: 11,
+    name: "ahihi",
+  },
+  {
+    id: 22,
+    name: "ahoho",
+  },
+  {
+    id: 33,
+    name: "kiki",
+  },
+  {
+    id: 44,
+    name: "kaka",
+  },
+  {
+    id: 55,
+    name: "hehe",
+  },
+];
 function App() {
+  const [val, setVal] = useState([]);
+
+  const handlevalue = () => {
+    console.log(val);
+  };
+  const handlechange = (e) => {
+    if (!val.includes(e)) {
+      setVal([...val, e]);
+    } else {
+      setVal(
+        val.filter((i) => 
+          i.id !== e.id
+        )
+      );
+    }
+  };
+  const handleAllvalue = () => {
+    let arr_id = [];
+    arr_id = api.map((item) => {
+      return item.id;
+    });
+    setVal(arr_id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 20 }}>
+      <h1>{JSON.stringify(val)}</h1>
+      {api.map((item) => (
+        <div key={item.id}>
+          <input type="checkbox" onChange={() => handlechange(item)} />
+          {item.name}
+        </div>
+      ))}
+
+      <button onClick={handlevalue}>Get Value</button>
+      <button onClick={handleAllvalue}>Select ALL</button>
     </div>
   );
 }
